d9ab1850f8ed8f42eb417cf84bbd1d2d9f3c374f
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-20 20:43:20 -0300

    Update ContatosController.php

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -1,20 +1,72 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Contato;
+use PhpParser\Node\Expr\AssignOp\Concat;
 
 class ContatosController extends Controller
 {
-    public function index(Request $request) {
+    public function index2(Request $request) {
 
-        $contato = $request->all();
-        $email = $contato['emailContato'];
-        $tipo = $contato['tipoContato'];
-        $mensagem = $contato['mensagemContato'];
+        //opcao1 (get)
+        // $contato = [
+        //     'email'=>$request->query('emailContato'),
+        //     'tipo'=>$request->query('tipoContato'),
+        //     'mensagem'=>$request->query('mensagemContato')
+        // ];
+
+        // return view('site.contatos', $contato);
+
+        //opcao2 (post)
+        // $contato = [
+        //     'email'=>$request->input('emailContato'),
+        //     'tipo'=>$request->input('tipoContato'),
+        //     'mensagem'=>$request->input('mensagemContato')
+        // ];
+
+        // return view('site.contatos', $contato);
+
+        //opcao3 (post)
+        // $contato = [
+        //     'email'=>$request->input('emailContato'),
+        //     'tipo'=>$request->input('tipoContato'),
+        //     'mensagem'=>$request->input('mensagemContato')
+        // ];
+
+        // return view('site.contatos', $contato);
+
+        //opcao4 (post)
+        $email = $request->input('emailContato');
+        $tipo = $request->input('tipoContato');
+        $mensagem = $request->input('mensagemContato');
+
+        return view('site.contatos', compact('email','tipo','mensagem'));
+    }
+
+    public function index() {
+        $contato = Contato::all();
+
+        return view('site.contatos', ['contato' => $contato]);
+    }
+
+    public function store(Request $request) {
+        $contato = new Contato;
+
+        $contato->nome = $request->nomeContato;;
+        $contato->email = $request->emailContato;
+        $contato->tipo = $request->tipoContato;
+        $contato->mensagem = $request->mensagemContato;
 
 
-        return view('site.contatos', 
-        ['email'=>$email, 'tipo'=>$tipo, 'mensagem'=>$mensagem]);       
+
+        try {
+            $contato->save();
+
+            return redirect()->action([ContatosController::class, 'index'])
+                                    ->with('msg','Mensagem enviada com sucesso!');
+        } catch (\Exception $e) {
+            return redirect()->action([ContatosController::class, 'index'])
+                                    ->with('msg','Falha no envio da mensagem!');
+        }
     }
 }
