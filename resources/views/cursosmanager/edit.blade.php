7d1b5be0aad8e741aaa028afcac5782f2c13f3fe
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-24 12:20:53 -0300

    Update edit.blade.php

--- resources/views/cursosmanager/edit.blade.php
+++ resources/views/cursosmanager/edit.blade.php
@@ -2,51 +2,52 @@
 
 @section('content')
 <div class="jumbotron">
-    <h1 class="display-4">Projeto Laravel - Cadastro de Cursos</h1>
+    <h1 class="display-4">Projeto Laravel - Editar Curso</h1>
     <hr class="my-4">
 </div>
 <div class="container">
-    <a class="btn btn-success" href="{{ route('cursosmanager.create') }}">Criar Novo Curso</a>
+    <a class="btn btn-primary" href="{{ route('cursosmanager.index') }}"> Voltar</a>
     <p></p>
-    @if ($message = Session::get('success'))
+    @if ($errors->any())
         <p></p>
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
+        <div class="alert alert-danger">
+            <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
     @endif
 
-    <table class="table table-bordered">
-        <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Descrição</th>
-            <th>Imagem</th>
-            <th width="280px">Ação</th>
-        </tr>
-        @foreach ($cursos as $curso)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $curso->nome }}</td>
-            <td>{{ $curso->descricao }}</td>
-            <td>{{ $curso->imagem }}</td>
-            <td>
-                <form action="{{ route('cursosmanager.destroy', $curso->id) }}" method="POST">
+    <form action="{{ route('cursosmanager.update', $curso) }}" method="POST">
+        @csrf
+        @method('PUT')
 
-                    <a class="btn btn-info" href="{{ route('cursosmanager.show', $curso->id) }}">Exibir</a>
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nome:</strong>
+                    <input type="input" name="nome" value="{{ $curso->nome }}" class="form-control" placeholder="Nome">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Descrição:</strong>
+                    <textarea class="form-control" style="height:150px" name="descricao" placeholder="Descrição">{{ $curso->descricao }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Imagem:</strong>
+                    <input type="input" name="imagem" value="{{ $curso->imagem }}" class="form-control" placeholder="Imagem">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+        </div>
 
-                    <a class="btn btn-primary" href="{{ route('cursosmanager.edit', $curso->id) }}">Editar</a>
-
-                    @csrf
-                    @method('DELETE')
-
-                    <button type="submit" class="btn btn-danger">Excluir</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
-    {!! $cursos->links() !!}
+    </form>
 </div>
-
 @endsection
