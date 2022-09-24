cd237b296560280f490f4e222fdaa4b89e162673
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-24 11:26:40 -0300

    Aula2409

--- app/Models/contato.php
+++ app/Models/contato.php
@@ -1,8 +1,14 @@
+<?php
+
+namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Contato extend Model {
+class Contato extends Model
+{
     use HasFactory;
+
     protected $fillable = [
         'nome',
         'email',
